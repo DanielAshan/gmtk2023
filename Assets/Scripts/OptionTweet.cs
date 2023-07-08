9a44699159babe2f0f35321d2ca516ff413539be
Hamfrej <pieczynski.daniel@gmail.com>
2023-07-08 11:30:25 +0200

    Basic tweet generation

---
+++ Assets/Scripts/OptionTweet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionTweet : MonoBehaviour
+{
+    [SerializeField] public RawImage profilePicture;
+    [SerializeField] public TextMeshProUGUI userName;
+    [SerializeField] public TextMeshProUGUI userHandle;
+    [SerializeField] public TextMeshProUGUI tweetText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
