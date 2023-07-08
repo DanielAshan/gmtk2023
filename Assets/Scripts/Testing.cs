c37ed57e05a3059cd20c3a34a251af65f6c15db0
Hamfrej <pieczynski.daniel@gmail.com>
2023-07-08 09:44:10 +0200

    Test changes

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    [SerializeField] Transform  testTransform;
     // Start is called before the first frame update
     void Start()
     {
