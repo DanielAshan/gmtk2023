466a6387536a5267160565f5bb157c3ec5853544
vconsellation <vlightingdust@gmail.com>
2023-07-08 16:53:36 +0200

    Start work on json loader

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,10 @@
+[System.Serializable]
+public class Target
+{
+    public string name;
+    public string desc;
+
+    public string[] traits;
+
+}
+
---
+++ Assets/Scripts/Targets.cs
@@ -0,0 +1,5 @@
+[System.Serializable]
+public class Targets
+{
+    public Target[] targets;
+}
