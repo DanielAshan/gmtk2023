87d01851a9bb18736c72e4b8d311099b44256ffe
vconsellation <vlightingdust@gmail.com>
2023-07-08 11:32:08 +0200

    Add some stuff

---
+++ Assets/Scripts/UserProfile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserProfile : MonoBehaviour
+{
+
+    public string userHandle;
+    public string traits;
+}
