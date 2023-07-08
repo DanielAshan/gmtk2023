438157dbd7e9054ca74793f18bb48d087b3826c7
vconsellation <vlightingdust@gmail.com>
2023-07-08 17:35:15 +0200

    Start work on text json loader

--- Assets/Scripts/JLo.cs
+++ Assets/Scripts/JLo.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class JLo : MonoBehaviour
 {
-    public TextAsset jsonFile;
+    public TextAsset targetJsonFile;
+    public TextAsset tweetsJsonFile;
 
     void Start()
     {
-        Targets targetsInJson = JsonUtility.FromJson<Targets>(jsonFile.text);
+        Targets targetsInJson = JsonUtility.FromJson<Targets>(targetJsonFile.text);
 
         foreach (Target target in targetsInJson.targets)
         {
             Debug.Log(target.name);
             Debug.Log(target.desc);
         }
+
+        Debug.Log("LOADING TWEETS NOW");
+
+        LoadTweets();
     }
+
+    void LoadTweets()
+    {
+        Tweets tweetsInJson = JsonUtility.FromJson<Tweets>(tweetsJsonFile.text);
+
+        foreach (Twit tweet in tweetsInJson.tweets)
+        {
+            // Debug.Log(tweet.name);
+            Debug.Log(tweet.content);
+        }
+    }
+
 }
