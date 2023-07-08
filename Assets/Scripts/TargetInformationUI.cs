5e64ab43f9ffb0deb154e61ce67aa5d399c3d689
vconsellation <vlightingdust@gmail.com>
2023-07-08 15:15:35 +0200

    Add end turn button, rework various UI elements

--- Assets/Scripts/TargetInformationUI.cs
+++ Assets/Scripts/TargetInformationUI.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+
 
 public class TargetInformationUI : MonoBehaviour
 {
@@ -9,10 +11,21 @@ public class TargetInformationUI : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI traitsText;
 
+    [SerializeField]
+    public RawImage avatar;
+
+    Texture2D texture;
+
     void Start()
     {
         handleText.text = "This is a test text";
 
         traitsText.text = " - This is a first trait \n - This is a second trait \n - And this is a third trait";
+
+        texture = Resources.Load("hrld") as Texture2D;
+
+        Debug.Log(texture);
+
+        avatar.GetComponent<RawImage>().texture = texture;
     }
 }
