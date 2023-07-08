fd251d062750c1c092ac9b018f22e0922f2329c6
vconsellation <vlightingdust@gmail.com>
2023-07-08 20:17:41 +0200

    Add shuffle array method

--- Assets/Scripts/JLo.cs
+++ Assets/Scripts/JLo.cs
@@ -7,28 +7,18 @@ public class JLo : MonoBehaviour
     public TextAsset targetJsonFile;
     public TextAsset tweetsJsonFile;
 
-    public void LoadTargets()
+    public Targets LoadTargets()
     {
         Targets targetsInJson = JsonUtility.FromJson<Targets>(targetJsonFile.text);
 
-        foreach (Target target in targetsInJson.targets)
-        {
-            Debug.Log(target.name);
-            Debug.Log(target.desc);
-        }
-
-        Debug.Log("LOADING TWEETS NOW");
+        return targetsInJson;
     }
 
-    public void LoadTweets()
+    public Tweets LoadTweets()
     {
         Tweets tweetsInJson = JsonUtility.FromJson<Tweets>(tweetsJsonFile.text);
 
-        foreach (Twit tweet in tweetsInJson.tweets)
-        {
-            // Debug.Log(tweet.name);
-            Debug.Log(tweet.content);
-        }
+        return tweetsInJson;
     }
 
 }
--- Assets/Scripts/Targets/TargetDB.cs
+++ Assets/Scripts/Targets/TargetDB.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 using UnityEngine;
 
 public class TargetDB : MonoBehaviour
 {
     public JLo jloader;
 
+    public Targets loadedTargets;
+
+    
+
     void Start()
     {
-        // JLo jloader = GetComponent<JLo>();
 
-        jloader.LoadTargets(); 
+
+        loadedTargets = jloader.LoadTargets(); 
+
+
+        System.Random random = new System.Random();
+        loadedTargets.targets = loadedTargets.targets.OrderBy(x => random.Next()).ToArray();
+        foreach (var i in loadedTargets.targets)
+        {
+            Debug.Log(i.name);
+        }
+    
     }
+
 }
